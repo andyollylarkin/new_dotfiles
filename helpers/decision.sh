2f3284169214739ccbfee2db350e7b41cbe808a5
Denis Davydov <follenlast2@gmail.com>
2022-06-18 16:50:10 +0300

    helper functions and inputrc

---
+++ helpers/decision.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+function decision(){
+    declare -A rand_res;
+    local decision=$((1 + $RANDOM % 10));
+    for i in {1..11}; do
+        rand_res[$i]=$((1 + $RANDOM % 10));
+    done
+    for i in ${rand_res[@]}; do
+        decision=$i;
+    done
+    if [ $decision -gt 5 ]; then echo 'Yes'; else echo 'No'; fi
+}
