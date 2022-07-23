c8df9885164dd1517a8f3d7658daff66c367f4e4
Denis Davydov <follenlast2@gmail.com>
2022-07-23 09:06:28 +0300

    vscode settings

--- helpers/helpers.sh
+++ helpers/helpers.sh
@@ -5,7 +5,7 @@ get_pass() {
     xclip --help 2>/dev/null;
     local exist_status=$?;
     if [ $exist_status == 127 ]; then
-        echo -e "\e[41mINSTALLING XCLIP\e[0m"
+        echo -e "\e[41mINSTALLING XCLIP\e[0m\n"
         sudo apt-get install -qqy xclip;
     fi
     echo -n ${1}|md5sum|grep -Eo '([A-Z]|[a-z]|[0-9])+'|tr -d "\n"|xclip -selection clipboard;
