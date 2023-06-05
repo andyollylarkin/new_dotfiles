174e2ae4ea0d50cb06123893839960f22c579add
Denis Davydov <follenlast2@gmail.com>
2023-06-05 15:22:53 +0300

    aliases

--- aliases/workaliases.sh
+++ aliases/workaliases.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 alias dep_prod="dep deploy --hosts='prod' --branch='master'"
+alias sshi="ssh -o IdentitiesOnly=yes"
+alias workssh="sshpass -p 12344 ssh -o IdentitiesOnly=yes $1"
